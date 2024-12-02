7127e380b1bd556dad4de10a9560e52dcb936335
Rahul Yadav <rahulydv1227@gmail.com>
2024-12-02 13:09:31 +0530

    third project

---
+++ 02tailwindcss/src/App.jsx
@@ -0,0 +1,28 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  
+  
+
+  return (
+    <>
+    <h2 className='bg-green-400 text-black p-4 font-bold mb-3'>Welcome to DJ Party</h2>
+    <div className='flex p-4 space-x-2 align-middle '>
+    <Card singername="Naina" song="Arijit"/>
+    <Card singername="" song="Sonu"/>
+    <Card singername="" song="Mika"/>
+    
+
+   
+    
+    
+    </div>
+    
+      
+    </>
+  )
+}
+
+export default App
